b6ef57d096341f6c734a771af4591b77029e869b
Riku Kojima <riku@RikunoMacBook-puro.local>
2018-11-05 15:24:23 +0900

    Fixed2

--- database/migrations/2018_09_20_012819_add_title_toquestions.php
+++ database/migrations/2018_09_20_012819_add_title_toquestions.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTitleToquestions extends Migration
+class AddMoreTitleToquestions extends Migration
 {
     /**
      * Run the migrations.
